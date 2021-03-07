1e44a307a38f0e43688170f7948454f20ae1ad3f
hamachiyo <autumn.c.6105503@gmail.com>
2021-03-07 15:43:58 +0900

    remove step4 and step5

--- routes/skyway.js
+++ routes/skyway.js
@@ -13,11 +13,5 @@ router.get('/step2', function (req, res, next) {
 router.get('/step3', function (req, res, next) {
   res.render('step3', { title: 'Step3' });
 });
-router.get('/step4', function (req, res, next) {
-  res.render('step4', { title: 'Step4' });
-});
-router.get('/step5', function (req, res, next) {
-  res.render('step5', { title: 'Step5' });
-});
 
 module.exports = router;
