a9b3960714e5e698fe9ea42d467d9e00d4346d96
hamachiyo <autumn.c.6105503@gmail.com>
2021-03-07 14:30:00 +0900

    skyway step2

--- public/javascripts/step2.js
+++ public/javascripts/step2.js
@@ -1,53 +1,44 @@
 (async function main() {
   let localStream = null;
   let peer = null;
-  let existingCall = null;
-
-  function setupCallEventHandlers(call) {
-    if (existingCall) {
-      existingCall.close();
-    };
-
-    existingCall = call;
-
-    //データ受信時の処理
-    call.on('stream', function (stream) {
-      addVideo(call, stream);
-      setupEndCallUI();
-      $('#connected-peer-id').text(call.remoteId);
-    });
-
-    //切断時の処理
-    call.on('close', function () {
-      removeVideo(call.remoteId);
-      setupMakeCallUI();
-    });
-  }
-
-  function addVideo(call, stream) {
-    const videoDom = $('<video autoplay>');
-    videoDom.attr('id', call.remoteId);
-    videoDom.get(0).srcObject = stream;
-    $('.videosContainer').append(videoDom);
-  }
-
-  function removeVideo(peerId) {
-    $('#' + peerId).remove();
-  }
-
-  function setupMakeCallUI() {
-    $('#make-call').show();
-    $('#end-call').hide();
-  }
-
-  function setupEndCallUI() {
-    $('#make-call').hide();
-    $('#end-call').show();
-  }
-
-
 
   const localVideo = document.getElementById('js-local-video');
+  const localId = document.getElementById('js-local-id');
+  const remoteVideo = document.getElementById('js-remote-video');
+  const remoteId = document.getElementById('js-remote-id');
+  const connectedId = document.getElementById('js-connected-id');
+  const callTrigger = document.getElementById('js-call-trigger');
+  const closeTrigger = document.getElementById('js-close-trigger');
+
+  //受電及び打電時に行う共通処理
+  function setupCallEventHandlers(mediaConnection) {
+    //接続中のID表示
+    connectedId.textContent = mediaConnection.remoteId;
+
+    //「コネクションが繋がったら、接続相手のストリームを表示し続ける」という処理を追加
+    //on:〜し続ける（だと思う）
+    mediaConnection.on('stream', stream => {
+      remoteVideo.srcObject = stream;
+    });
+
+    //「切断するときは、もろもろ綺麗にする」という処理を追加
+    //once:一度だけ〜する（だと思う）
+    mediaConnection.once('close', () => {
+      //全てのトラックの停止
+      remoteVideo.srcObject.getTracks().forEach(track => {
+        track.stop();
+      });
+
+      //表示に利用したDOMの初期化
+      remoteVideo.srcObject = null;
+      connectedId.textContent = '...';
+    });
+
+    //停止ボタンに「コネクションの切断」する処理を追加
+    closeTrigger.addEventListener('click', () => {
+      mediaConnection.close(true);
+    });
+  }
 
   localStream = await navigator.mediaDevices.getUserMedia({
     audio: true,
@@ -62,39 +53,37 @@
       }
     }
   });
+  localVideo.srcObject = localStream;
 
   peer = new Peer({
     key: '6b3172b3-ae11-456d-9049-699e0f260a49', //API-key
     debug: 3 //ログレベル：完全
   });
 
-  //サーバーとの接続成功時にapi-key表示
-  peer.on('open', function () {
-    $('#my-id').text(peer.id);
+  //サーバーとの接続成功時にこちらのid表示
+  peer.on('open', (id) => {
+    localId.textContent = id;
   });
 
-  //相手からの接続要求時の処理：単純な応答処理（必須っぽいし、わざわざ各自で記載する必要ってあるんか？）
-  peer.on('call', function (call) {
-    call.answer(localStream);
-    setupCallEventHandlers(call);
+  //受電処理：相手からの接続要求時の処理
+  peer.on('call', mediaConnection => {
+    //応答処理
+    mediaConnection.answer(localStream);
+
+    //共通処理の実行
+    setupCallEventHandlers(mediaConnection);
   });
 
-  //エラー発生時にエラーを表示する
-  peer.on('error', function (err) {
-    alert(err.message);
+  //打電処理：こちらから通話を始めるときの処理（打電ボタンへのイベント追加）
+  callTrigger.addEventListener('click', () => {
+    //peer通話を開始する
+    const mediaConnection = peer.call(remoteId.value, localStream);
+
+    //共通処理の実行
+    setupCallEventHandlers(mediaConnection);
   });
 
-  //発信処理
-  $('#make-call').submit(function (e) {
-    e.preventDefault();
-    const call = peer.call($('#peer-id').val(), localStream);
-    setupCallEventHandlers(call);
-  });
+  //エラー発生時にコンソールにエラーを表示する
+  peer.on('error', console.error);
 
-  //切断処理
-  $('#end-call').click(function () {
-    existingCall.close();
-  });
-
-  localVideo.srcObject = localStream;
 })();
